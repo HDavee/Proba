f590ed8a6b530ac130be033ddd6c1a6e0941120c
Hajcsár Dávid <hajcsardavid@gmail.com>
2024-02-11 16:59:46 +0100

    Módosítottam a kiírt szöveget

--- fajlkezeles2.py
+++ fajlkezeles2.py
@@ -5,7 +5,8 @@ with open('fajl.txt', "r") as forrasfajl:
         adatok = sor.strip(";")
         szo = {f'évszám : {adatok[0]}, első : {adatok[1]}, második : {adatok[2]}, harmadik : {adatok[3]}'}
         szoveg.append(szo)
-        
-print(szoveg)
+
+#print(szoveg)
+print(f"Ez egy jó megoldás{szoveg}")
 #for sor in forrasfajl:
 #    print(sor)
